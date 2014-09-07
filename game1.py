cab3b93f7fc121a87b4e180f586fd1270903e8a5
Jackie Loven <jloven@wpi.edu>
2014-09-06 22:58:31 -0400

    Added hobbies question and parsing for 'to hobby' vs 'hobbying'

--- game1.py
+++ game1.py
@@ -4,5 +4,47 @@
 
 """This is my first trial of a Python script."""
 
+# Functions
+def isItYes(user_input):
+    if user_input[:2] == "Ye" or user_input[:2] == "ye":
+        return 1
+    else:
+        return 0
+
+
+def youLikeTo(user_input):
+    i_like, like_, to_hobby = user_input.partition("like ")
+    return to_hobby
+
+def youLike(user_input):
+    i_like, like_, hobbying = user_input.partition("like ")
+    return hobbying
+
+def hobbyInputParse(user_input):
+    if "ing" in user_input:
+            return youLike(user_input)
+    else:
+        return youLikeTo(user_input)
+
+
+# User-Input Variables
 input_name = raw_input("Hi! What's your name? ")
-print "It's nice to meet you,", input_name
+print ("It\'s nice to meet you, {}!".format(input_name))
+
+input_hobby_yn = raw_input("Do you have any hobbies? ")
+
+
+# Computer Responses
+if isItYes(input_hobby_yn) == 1:
+    print ("That\'s so cool! I like juggling.")
+else:
+    print ("Aw, come on! Don't you like cooking or music or doodling?")
+
+
+input_hobby = raw_input("What do you like to do? ")
+print ("Wow! You like {}?".format(hobbyInputParse(input_hobby)))
+
+
+
+
+
